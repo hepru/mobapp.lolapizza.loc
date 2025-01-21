19484cfbfb0627ad2ffef4870acf5475758fec34
Evgenei <hep1984@gmail.com>
2025-01-21 12:43:39 +0300

    Метрика добавлена

--- index.php
+++ index.php
@@ -53,5 +53,22 @@
 <script src="js/jquery.validate.min.js?v=1.01"></script>
 <script src="js/jquery.mask.min.js?ver=1.01"></script>
 <script src="js/main.js?v=1.04"></script>*/?>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(99257716, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/99257716" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 </body>
 </html>
